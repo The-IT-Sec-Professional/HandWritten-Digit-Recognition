148c5fcb92d19ec8659b2ca0c04e0eeab8ec43c6
William Jhan Paul Armstrong <91146497+The-IT-Sec-Professional@users.noreply.github.com>
2024-02-18 16:54:43 -0500

    Update readit.py
    Code Fixes:
    more neurons for accuracy.
    Dropout layers.
    another final dense layer.
    more epochs.
    validation check
    
    Signed-off-by: William Jhan Paul Armstrong <91146497+The-IT-Sec-Professional@users.noreply.github.com>

--- readit.py
+++ readit.py
@@ -12,24 +12,21 @@ X_test = tf.keras.utils.normalize(X_test, axis=1)
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
 
-model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
-
-model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
+model.add(tf.keras.layers.Dense(units=512, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=512, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dropout(0.2))  # Dropout layer
+model.add(tf.keras.layers.Dense(units=100, activation=tf.nn.softmax))
 
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
-model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
-
-# Added code
-
-# end
-
+model.add(tf.keras.layers.Dense(units=512, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=512, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dropout(0.2))  # Dropout layer
+model.add(tf.keras.layers.Dense(units=100, activation=tf.nn.softmax))
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(X_train, y_train, epochs=3)
+model.fit(X_train, y_train, epochs=30)
 
 loss, accuracy= model.evaluate(X_test, y_test)
 print(loss)
@@ -40,9 +37,11 @@ model.save('digits.model')
 model = tf.keras.models.load_model('digits.model')
 
 img = cv2.imread('digit.png')[:,:,0]
+# Validation Check
+assert img is not None, "Image couldn't be read"
 img = np.invert(np.array([img]))
 
-prediction =model.predict(img)
+prediction = model.predict(img)
 print("Prediction:{}".format(np.argmax(prediction)))
 plt.imshow(img[0])
 plt.show()
